a9182656f5d737ad8cd01be99b8550584b42a802
Deepak Saini <deepak1307saini@gmail.com>
2023-03-15 11:45:54 +0530

    Initialization

---
+++ src/main/java/com/example/moviereviewSpringBootApplicaion/MovieReviewSpringBootApplicaionApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviereviewSpringBootApplicaion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewSpringBootApplicaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewSpringBootApplicaionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moviereviewSpringBootApplicaion/MovieReviewSpringBootApplicaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviereviewSpringBootApplicaion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewSpringBootApplicaionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
